214152033099a6d4a967534ffe9541694c15a983
Antonielson Silva <antonielsondev@gmail.com>
2024-07-07 04:08:19 -0300

    salvando alterações do postgress

--- AplicacaoDjango/settings.py
+++ AplicacaoDjango/settings.py
@@ -76,13 +76,26 @@ WSGI_APPLICATION = 'AplicacaoDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'receitas',
+        'USER': 'antonio',
+        'PASSWORD': 'Acido4tia1azabi-',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

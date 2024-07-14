62a5441fa5ec3537699ce412b259146e77d3c003
Antonielson Silva <antonielsondev@gmail.com>
2024-07-14 19:16:59 -0300

    Aplicando teste

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django import reverse
+
 
 # Create your tests here.
 
@@ -9,4 +11,10 @@ from django.test import TestCase
 
 class ReceitasURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        print('teste Receitas')
+        assert 1 == 1, 'Um é igual a um'
+        
+    # colocar nomes bem explicativos
+    def test_receitas_home_url_is_correct(self):
+        home_url = reverse('receitas:home')
+        self.assertEqual(home_url, '/')
